14ac0fd4a6c8f6359263610488bf01341ca50a1d
Hitaishi Kasinath <hitaishi@retailsuccess.com>
2020-09-16 22:29:47 -0500

    date time file

---
+++ DateTime.py
@@ -0,0 +1,7 @@
+import datetime
+presentDate = datetime.datetime.now()
+todayDate = datetime.datetime(2020, 9, 16)
+print(presentDate)
+print(todayDate)
+# or print this way
+print("Today's date and time:", presentDate, todayDate)
